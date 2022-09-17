f52ca8e024ffce0ed1bc5023a01e02af44c60ba3
Daniel Cariatte <78773889+Cariatte@users.noreply.github.com>
2022-09-17 19:37:53 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
-import 'react-native-gesture-handler';
 import React from 'react';
+import RootNavigation from './navigation/RootNavigation';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import Home from './pages/Home'
-import Cadastro from './pages/Cadastro'
-import Login from './pages/Login'
-
-const Stack = createStackNavigator();
 
 export default function App() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerShown: false
-        }}>
-          <Stack.Screen name='Cadastro' component={Cadastro}/>
-          <Stack.Screen name='Login' component={Login}/>
-          <Stack.Screen name='Home' component={Home}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <RootNavigation/>
+    </NavigationContainer>
     );
 }
 
--- README.md
+++ README.md
@@ -5,5 +5,4 @@ Comandos para rodar o App:
 1 - npm install -g expo-cli
 2 - npm start
 3 - npm install --global yarn
-4 - npm install
-5 - expo start
+4 - expo start
